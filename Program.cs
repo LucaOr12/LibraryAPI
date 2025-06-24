006c7b928d463bbe31942f68ae725500be9ebf8f
Luca Orabona <102247682+LucaOr12@users.noreply.github.com>
2025-06-24 23:47:50 +0200

    check

--- Program.cs
+++ Program.cs
@@ -73,7 +73,11 @@ app.UseSwaggerUI(c =>
 app.MapOpenApi();
 
 app.UseCors("AllowFrontend");
-
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("Authorization header: " + context.Request.Headers["Authorization"].ToString());
+    await next();
+});
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
