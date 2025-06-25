5548546e7b844c62046eed54b9592be2e511fc46
Luca Orabona <102247682+LucaOr12@users.noreply.github.com>
2025-06-25 20:54:05 +0200

    changes

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -1,73 +1,73 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 using LibraryAPI.Data;
-using LibraryAPI.DTOs;
 using LibraryAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-
 
 namespace LibraryAPI.Controllers;
 
 [Authorize]
 [ApiController]
 [Route("api/[controller]")]
-
 public class ReservationsController : ControllerBase
 {
     private readonly LibraryContext _context;
     public ReservationsController(LibraryContext context) => _context = context;
-    
+
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Reservation>>> GetReservations() => await _context.Reservations.ToListAsync();
-    
+    public async Task<ActionResult<IEnumerable<Reservation>>> GetReservations()
+        => await _context.Reservations.ToListAsync();
+
     [HttpGet("{id}")]
-    public async Task <ActionResult<Reservation>> GetReservation(int id)
+    public async Task<ActionResult<Reservation>> GetReservation(int id)
     {
         var reservation = await _context.Reservations.FindAsync(id);
         return reservation == null ? NotFound() : Ok(reservation);
     }
-    [AllowAnonymous]
+
     [HttpPost]
-    public async Task<ActionResult<Reservation>> CreateReservation(ReservationCreateDTO dto)
+    public async Task<ActionResult<Reservation>> CreateReservation(Reservation incoming)
     {
         var userIdClaim = User.FindFirst("userId");
-        if(userIdClaim == null || !int.TryParse(userIdClaim.Value, out int userId))return Unauthorized();
-        Console.WriteLine($"UserId from token: {userIdClaim.Value}");
-        var book = await _context.Books.FindAsync(dto.BookId);
-        
-        if(book == null) return BadRequest("Invalid reservation");
+        if (userIdClaim == null || !int.TryParse(userIdClaim.Value, out int userId))
+            return Unauthorized();
+
+        var book = await _context.Books.FindAsync(incoming.BookId);
+        if (book == null) return BadRequest("Invalid book");
         if (!book.isAvailable) return BadRequest("Book is not available");
 
         var reservation = new Reservation
         {
-            BookId = dto.BookId,
+            BookId = incoming.BookId,
             UserId = userId,
-            ReservedAt = DateTime.UtcNow,
+            ReservedAt = DateTime.UtcNow
         };
-        
+
         _context.Reservations.Add(reservation);
         await _context.SaveChangesAsync();
-        
+
         return CreatedAtAction(nameof(GetReservation), new { id = reservation.Id }, reservation);
     }
-    
+
     [HttpPut("{id}")]
-    public async Task <IActionResult> Update(int id, Reservation reservation)
+    public async Task<IActionResult> Update(int id, Reservation reservation)
     {
         if (id != reservation.Id) return BadRequest();
-        var check = await _context.Reservations.AnyAsync(r => r.Id == id);
-        if(!check) return NotFound();
+        var exists = await _context.Reservations.AnyAsync(r => r.Id == id);
+        if (!exists) return NotFound();
+
         _context.Entry(reservation).State = EntityState.Modified;
         await _context.SaveChangesAsync();
         return NoContent();
     }
-    
+
     [HttpDelete("{id}")]
-    public async Task <IActionResult> Delete(int id)
+    public async Task<IActionResult> Delete(int id)
     {
         var reservation = await _context.Reservations.FindAsync(id);
         if (reservation == null) return NotFound();
+
         _context.Reservations.Remove(reservation);
         await _context.SaveChangesAsync();
         return NoContent();
