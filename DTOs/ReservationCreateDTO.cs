6297c366f92ed7e3eaf5163498e82aa626e1a17a
Luca Orabona <102247682+LucaOr12@users.noreply.github.com>
2025-06-25 21:03:55 +0200

    clean up

--- DTOs/ReservationCreateDTO.cs
+++
@@ -1,6 +0,0 @@
-namespace LibraryAPI.DTOs;
-
-public class ReservationCreateDTO
-{
-    public int BookId { get; set; }
-}
--- DTOs/ReservationDTO.cs
+++
@@ -1,6 +0,0 @@
-namespace LibraryAPI.DTOs;
-
-public class ReservationDTO
-{
-    
-}
